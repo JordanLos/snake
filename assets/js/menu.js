1f3c80e681e51c8d0d1a58ec0cbe86c4017917f4
Jordan Los <Jordan@Meaghans-MacBook-Air.local>
2016-02-06 14:12:48 -0700

    made unknown change

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,7 +4,7 @@ var Menu = {
 		// Images are loaded to create sprites later
 		// First argument is the name of the menu image
 		// Second argument is the path to the menu image file
-		game.load.image('menu', './assets/images/menu.jpg');
+		game.load.image('menu', './assets/images/menu.png');
 	},
 
 	create : function () {
